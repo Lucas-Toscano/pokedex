2125149b29887ea4a54e90108f477a13d8d30c31
Lucas-Toscano <lucas_toscano00@hotmail.com>
2022-07-26 22:48:20 -0300

    new home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,3 +68,14 @@ nextButton.addEventListener('click', () => {
 });
 
 renderPokemon(searchPokemon.toString());
+
+function Menu(e) {
+  let list = document.querySelector("ul");
+  e.name === "menu"
+    ? ((e.name = "close"),
+      list.classList.add("top-[80px]"),
+      list.classList.add("opacity-100"))
+    : ((e.name = "menu"),
+      list.classList.remove("top-[80px]"),
+      list.classList.remove("opacity-100"));
+}
